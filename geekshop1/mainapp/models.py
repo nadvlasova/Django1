eac59dfcc56b4eefe4d46069be5da11d222bcecc
Nadezhda <nadvlasova@rambler.ru>
2021-11-26 08:05:50 +0800

    migrate ProductCategory

--- geekshop1/mainapp/models.py
+++ geekshop1/mainapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True, null=True)
