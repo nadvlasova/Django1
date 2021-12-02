0577153676e7592be311138c76586ba74f96389d
Nadezhda <nadvlasova@rambler.ru>
2021-12-02 12:51:50 +0800

    paths ready

--- geekshop1/admins/views.py
+++ geekshop1/admins/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'admins/admin.html')
 
+
 def admin_users(request):
     return render(request, 'admins/admin-users-read.html')
+
 
 def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html')
 
+
 def admin_users_update(request):
     return render(request, 'admins/admin-users-update-delete.html')
+
 
 def admin_users_delete(request):
     return render(request, 'admins/admin-users-update-delete.html')
