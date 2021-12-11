e54cce498bd115a832c432771f4008b005e62898
Nadezhda <nadvlasova@rambler.ru>
2021-12-11 11:20:16 +0800

    authapp to CBV

--- geekshop1/authapp/views.py
+++ geekshop1/authapp/views.py
@@ -11,25 +11,6 @@ from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
 from authapp.models import User
 from baskets.models import Basket
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST.get('username')
-#             password = request.POST.get('password')
-#             user = auth.authenticate(username=username, password=password)
-#             if user.is_active:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('index'))
-#         # else:
-#         #     print(form.errors)
-#     else:
-#         form = UserLoginForm()
-#     context = {
-#         'title': 'Geekshop | Авторизация',
-#         'form': form
-#     }
-#     return render(request, 'authapp/login.html', context)
 from mainapp.mixin import BaseClassContextMixin, UserDispatchMixin
 
 
@@ -59,24 +40,6 @@ class RegisterListView(FormView, BaseClassContextMixin):
         return render(request, self.template_name, {'form': form})
 
 
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегистрировались!')
-#             return HttpResponseRedirect(reverse('authapp:login'))
-#         # else:
-#         #     print(form.errors)
-#     else:
-#         form = UserRegisterForm()
-#     context = {
-#         'title': 'Geekshop | Регистрация',
-#         'form': form
-#     }
-#     return render(request, 'authapp/register.html', context)
-
-
 class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
     template_name = 'authapp/profile.html'
     form_class = UserProfileForm
@@ -99,30 +62,5 @@ class ProfileFormView(UpdateView, BaseClassContextMixin, UserDispatchMixin):
         return context
 
 
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             messages.set_level(request, messages.SUCCESS)
-#             messages.success(request, 'Вы успешно сохранили профайл!')
-#             form.save()
-#         else:
-#             messages.set_level(request, messages.ERROR)
-#             messages.error(request, form.errors)
-#
-#
-#     context = {
-#         'title': 'Geekshop | Профайл',
-#         'form': UserProfileForm(instance=request.user),
-#         'baskets': Basket.objects.filter(user=request.user)
-#     }
-#     return render(request, 'authapp/profile.html', context)
-
-
 class Logout(LogoutView):
     template_name = 'mainapp/index.html'
-
-# def logout(request):
-#     auth.logout(request)
-#     return render(request, 'mainapp/index.html')
