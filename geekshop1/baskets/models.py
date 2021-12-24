668539dd635f2183ccdef1fafe7e48fe627a9d3f
Nadezhda <nadvlasova@rambler.ru>
2021-12-24 13:51:43 +0800

    baskets and orders quantity ready

--- geekshop1/baskets/models.py
+++ geekshop1/baskets/models.py
@@ -6,7 +6,17 @@ from authapp.models import User
 from mainapp.models import Product
 
 
+# class BasketQuerySet(models.QuerySet):
+#     def delete(self, *args, **kwargs):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
+    # objects = BasketQuerySet.as_manager()
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
@@ -14,8 +24,7 @@ class Basket(models.Model):
     update_timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Корзина для {self.user.username} | Продукт {self.product.name}'
-
+        return f'Корзина для  {self.user.username} | Продукт{self.product.name}'
 
     def sum(self):
         return self.quantity * self.product.price
@@ -27,3 +36,22 @@ class Basket(models.Model):
     def total_quantity(self):
         baskets = Basket.objects.filter(user=self.user)
         return sum(basket.quantity for basket in baskets)
+
+    # def delete(self, *args, **kwargs):
+    #     self.product.quantity += self.quantity
+    #     self.save()
+    #     super(Basket, self).delete(*args, **kwargs)
+    #
+    # def save(self, *args, **kwargs):
+    #
+    #     if self.pk:
+    #         get_item = self.get_item(int(self.pk))
+    #         self.product.quantity -= self.quantity - get_item
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save(*args, **kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
