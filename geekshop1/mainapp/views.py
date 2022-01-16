db6bb101c9c80fb753baca3c5c27270b83500d92
Nadezhda <nadvlasova@rambler.ru>
2022-01-16 17:39:53 +0800

    cache all site

--- geekshop1/mainapp/views.py
+++ geekshop1/mainapp/views.py
@@ -60,7 +60,7 @@ def index(request):
     return render(request, 'mainapp/index.html', context)
 
 
-@cache_page (3600)
+# @cache_page (3600)
 def products(request, id_category=None, page=1):
     context = {
         'title': 'Geekshop | Каталог',
