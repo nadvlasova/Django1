7837fcbbd855585e68de4b3aab7435c8b992f96b
Nadezhda <nadvlasova@rambler.ru>
2022-01-20 14:07:32 +0800

    to server

--- geekshop1/authapp/pipelines.py
+++ geekshop1/authapp/pipelines.py
@@ -13,8 +13,8 @@ def save_user_profile(backend, user, response, *args, **kwargs):
         return
 
     api_url = urlunparse(('http', 'api.vk.com', 'method/users.get', None,
-                         urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about','photo_200','personal')),
-                                               access_token=response['access_token'], v=5.131)), None))
+                          urlencode(OrderedDict(fields=','.join(('bdate', 'sex', 'about', 'photo_200', 'personal')),
+                                                access_token=response['access_token'], v=5.131)), None))
 
     resp = requests.get(api_url)
     if resp.status_code != 200:
@@ -32,25 +32,24 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if data['about']:
         user.userprofile.about = data['about']
 
-    bdate = datetime.strptime(data['bdate'],'%d.%m.%Y').date()
+    bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
 
     age = timezone.now().date().year - bdate.year
 
     user.age = age
     if age < 18:
         user.delete()
-        raise  AuthForbidden('social_core.backends.vk.VKOAuth2')
+        raise AuthForbidden('social_core.backends.vk.VKOAuth2')
 
     if data['photo_200']:
-        photo_link =  data['photo_200']
+        photo_link = data['photo_200']
         photo_response = requests.get(photo_link)
         path_photo = f'users_image/{user.pk}.jpg'
-        with open(f'media/{path_photo}','wb') as photo:
+        with open(f'media/{path_photo}', 'wb') as photo:
             photo.write(photo_response.content)
         user.image = path_photo
 
     if data['personal']['langs']:
         user.userprofile.langs = data['personal']['langs'][0] if len(data['personal']['langs'][0]) > 0 else 'EN'
-
 
     user.save()
--- geekshop1/geekshop1/settings.py
+++ geekshop1/geekshop1/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     'admins',
     'social_django',
     'ordersapp',
-    # 'debug_toolbar',
-    # 'template_profiler_panel',
+    'debug_toolbar',
+    'template_profiler_panel',
     'django_extensions',
 ]
 
@@ -56,13 +56,13 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
-     #'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
     'social_django.middleware.SocialAuthExceptionMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
@@ -95,20 +95,20 @@ WSGI_APPLICATION = 'geekshop1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'geekshop',
-#         'USER': 'postgres',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -155,8 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',) # static for local
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') # static for server
+# STATICFILES_DIRS = (BASE_DIR / 'static',) # static for local
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') # static for server
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
@@ -212,31 +212,31 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
 )
 
-# if DEBUG:
-#     def show_toolbar(request):
-#         return True
-#
-#
-#     DEBUG_TOOLBAR_CONFIG = {
-#         'SHOW_TOOLBAR_CALLBACK': show_toolbar,
-#     }
-#
-#     DEBUG_TOOLBAR_PANELS = [
-#         'debug_toolbar.panels.versions.VersionsPanel',
-#         'debug_toolbar.panels.timer.TimerPanel',
-#         'debug_toolbar.panels.settings.SettingsPanel',
-#         'debug_toolbar.panels.headers.HeadersPanel',
-#         'debug_toolbar.panels.request.RequestPanel',
-#         'debug_toolbar.panels.sql.SQLPanel',
-#         'debug_toolbar.panels.templates.TemplatesPanel',
-#         'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-#         'debug_toolbar.panels.cache.CachePanel',
-#         'debug_toolbar.panels.signals.SignalsPanel',
-#         'debug_toolbar.panels.logging.LoggingPanel',
-#         'debug_toolbar.panels.redirects.RedirectsPanel',
-#         'debug_toolbar.panels.profiling.ProfilingPanel',
-#         'template_profiler_panel.panels.template.TemplateProfilerPanel',
-#     ]
+if DEBUG:
+    def show_toolbar(request):
+        return True
+
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'SHOW_TOOLBAR_CALLBACK': show_toolbar,
+    }
+
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'template_profiler_panel.panels.template.TemplateProfilerPanel',
+    ]
 
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 120
@@ -245,8 +245,8 @@ CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
 CACHES = {
     'default':{
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',  # для локальной
-        # 'LOCATION': '89.108.103.175:11211',  # для сервера
+        # 'LOCATION': '127.0.0.1:11211',  # для локальной
+        'LOCATION': '89.108.103.175:11211',  # для сервера
     }
 }
 
